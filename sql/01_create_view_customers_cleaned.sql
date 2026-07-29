bd45cf935816657cada04723b6f44e9602c5f5a0
MatheusAugustoEC <mtaugustoec@gmail.com>
2026-07-29 19:13:10 -0300

    add SQL view customers_cleaned

---
+++ sql/01_create_view_customers_cleaned.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE VIEW `churn_dataset.customers_cleaned` AS
+SELECT
+  customerID,
+  gender,
+  CAST(SeniorCitizen AS BOOL) AS SeniorCitizen,
+  Partner,
+  Dependents,
+  tenure,
+  PhoneService,
+  CASE
+    WHEN MultipleLines = 'No phone service' THEN 'No'
+    ELSE MultipleLines
+  END AS MultipleLines,
+  InternetService,
+  CASE WHEN OnlineSecurity = 'No internet service' THEN 'No' ELSE OnlineSecurity END AS OnlineSecurity,
+  CASE WHEN OnlineBackup = 'No internet service' THEN 'No' ELSE OnlineBackup END AS OnlineBackup,
+  CASE WHEN DeviceProtection = 'No internet service' THEN 'No' ELSE DeviceProtection END AS DeviceProtection,
+  CASE WHEN TechSupport = 'No internet service' THEN 'No' ELSE TechSupport END AS TechSupport,
+  CASE WHEN StreamingTV = 'No internet service' THEN 'No' ELSE StreamingTV END AS StreamingTV,
+  CASE WHEN StreamingMovies = 'No internet service' THEN 'No' ELSE StreamingMovies END AS StreamingMovies,
+  Contract,
+  PaperlessBilling,
+  PaymentMethod,
+  MonthlyCharges,
+  SAFE_CAST(TotalCharges AS FLOAT64) AS TotalCharges,
+  CASE
+    WHEN tenure BETWEEN 0 AND 12 THEN '0-12 months'
+    WHEN tenure BETWEEN 13 AND 24 THEN '13-24 months'
+    WHEN tenure BETWEEN 25 AND 48 THEN '25-48 months'
+    WHEN tenure BETWEEN 49 AND 60 THEN '49-60 months'
+    ELSE '60+ months'
+  END AS tenure_group,
+  ROUND(SAFE_DIVIDE(SAFE_CAST(TotalCharges AS FLOAT64), tenure), 2) AS AvgMonthlySpend,
+  Churn
+FROM `churn_dataset.raw_customers`
