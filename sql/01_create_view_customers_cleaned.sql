633b2a5543a7b2daecf3a7c0da95f9e8bb564724
MatheusAugustoEC <mtaugustoec@gmail.com>
2026-08-03 18:05:27 -0300

    add Docker configuration

--- sql/01_create_view_customers_cleaned.sql
+++ sql/01_create_view_customers_cleaned.sql
@@ -11,18 +11,21 @@ SELECT
     WHEN MultipleLines = 'No phone service' THEN 'No'
     ELSE MultipleLines
   END AS MultipleLines,
-  InternetService,
+  -- Substituindo espaços por underscore para consistência com o modelo
+  REPLACE(InternetService, ' ', '_') AS InternetService,
   CASE WHEN OnlineSecurity = 'No internet service' THEN 'No' ELSE OnlineSecurity END AS OnlineSecurity,
   CASE WHEN OnlineBackup = 'No internet service' THEN 'No' ELSE OnlineBackup END AS OnlineBackup,
   CASE WHEN DeviceProtection = 'No internet service' THEN 'No' ELSE DeviceProtection END AS DeviceProtection,
   CASE WHEN TechSupport = 'No internet service' THEN 'No' ELSE TechSupport END AS TechSupport,
   CASE WHEN StreamingTV = 'No internet service' THEN 'No' ELSE StreamingTV END AS StreamingTV,
   CASE WHEN StreamingMovies = 'No internet service' THEN 'No' ELSE StreamingMovies END AS StreamingMovies,
-  Contract,
+  -- Substituindo espaços por underscore para consistência com o modelo
+  REPLACE(Contract, ' ', '_') AS Contract,
   PaperlessBilling,
-  PaymentMethod,
+  -- Substituindo espaços, parênteses por underscore para consistência com o modelo
+  REPLACE(REPLACE(REPLACE(PaymentMethod, ' ', '_'), '(', ''), ')', '') AS PaymentMethod,
   MonthlyCharges,
-  SAFE_CAST(TotalCharges AS FLOAT64) AS TotalCharges,
+  COALESCE(SAFE_CAST(TotalCharges AS FLOAT64), 0) AS TotalCharges,
   CASE
     WHEN tenure BETWEEN 0 AND 12 THEN '0-12 months'
     WHEN tenure BETWEEN 13 AND 24 THEN '13-24 months'
@@ -30,6 +33,6 @@ SELECT
     WHEN tenure BETWEEN 49 AND 60 THEN '49-60 months'
     ELSE '60+ months'
   END AS tenure_group,
-  ROUND(SAFE_DIVIDE(SAFE_CAST(TotalCharges AS FLOAT64), tenure), 2) AS AvgMonthlySpend,
+  COALESCE(ROUND(SAFE_DIVIDE(SAFE_CAST(TotalCharges AS FLOAT64), tenure), 2), MonthlyCharges) AS AvgMonthlySpend,
   Churn
 FROM `churn_dataset.raw_customers`
