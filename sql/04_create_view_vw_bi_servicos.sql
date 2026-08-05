06e7559e411bcd9419262aeb60cdb81219ba6896
MatheusAugustoEC <mtaugustoec@gmail.com>
2026-08-05 17:26:26 -0300

    Atualizando sql

--- sql/04_create_view_vw_bi_servicos.sql
+++ sql/04_create_view_vw_bi_servicos.sql
@@ -1,15 +1,40 @@
-CREATE OR REPLACE VIEW `churn_dataset.vw_bi_servicos` AS
-SELECT
-  p.customerID,
-  p.churn_real,
-  p.risco,
-  CASE WHEN c.StreamingTV      = 'Yes' THEN 1 ELSE 0 END AS streaming_tv,
-  CASE WHEN c.StreamingMovies  = 'Yes' THEN 1 ELSE 0 END AS streaming_filmes,
-  CASE WHEN c.OnlineBackup     = 'Yes' THEN 1 ELSE 0 END AS backup_online,
-  CASE WHEN c.OnlineSecurity   = 'Yes' THEN 1 ELSE 0 END AS seguranca_online,
-  CASE WHEN c.TechSupport      = 'Yes' THEN 1 ELSE 0 END AS suporte_tecnico,
-  CASE WHEN c.DeviceProtection = 'Yes' THEN 1 ELSE 0 END AS protecao_dispositivo,
-  CASE WHEN c.MultipleLines    = 'Yes' THEN 1 ELSE 0 END AS multiplas_linhas
+CREATE OR REPLACE VIEW `churn_dataset.vw_bi_servicos_unpivot` AS
+SELECT p.customerID, p.churn_real, p.risco, 'Streaming TV' AS servico
 FROM `churn_dataset.churn_predictions` p
-LEFT JOIN `churn_dataset.customers_cleaned` c
-  ON p.customerID = c.customerID
+LEFT JOIN `churn_dataset.customers_cleaned` c ON p.customerID = c.customerID
+WHERE c.StreamingTV = 'Yes'
+
+UNION ALL
+
+SELECT p.customerID, p.churn_real, p.risco, 'Streaming Filmes' AS servico
+FROM `churn_dataset.churn_predictions` p
+LEFT JOIN `churn_dataset.customers_cleaned` c ON p.customerID = c.customerID
+WHERE c.StreamingMovies = 'Yes'
+
+UNION ALL
+
+SELECT p.customerID, p.churn_real, p.risco, 'Backup Online' AS servico
+FROM `churn_dataset.churn_predictions` p
+LEFT JOIN `churn_dataset.customers_cleaned` c ON p.customerID = c.customerID
+WHERE c.OnlineBackup = 'Yes'
+
+UNION ALL
+
+SELECT p.customerID, p.churn_real, p.risco, 'Segurança Online' AS servico
+FROM `churn_dataset.churn_predictions` p
+LEFT JOIN `churn_dataset.customers_cleaned` c ON p.customerID = c.customerID
+WHERE c.OnlineSecurity = 'Yes'
+
+UNION ALL
+
+SELECT p.customerID, p.churn_real, p.risco, 'Suporte Técnico' AS servico
+FROM `churn_dataset.churn_predictions` p
+LEFT JOIN `churn_dataset.customers_cleaned` c ON p.customerID = c.customerID
+WHERE c.TechSupport = 'Yes'
+
+UNION ALL
+
+SELECT p.customerID, p.churn_real, p.risco, 'Proteção de Dispositivo' AS servico
+FROM `churn_dataset.churn_predictions` p
+LEFT JOIN `churn_dataset.customers_cleaned` c ON p.customerID = c.customerID
+WHERE c.DeviceProtection = 'Yes'
